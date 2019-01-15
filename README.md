63a46864bd0062cfc422bb0a806f18cccbe07e02
Dennis Cope <dennisaaroncope@gmail.com>
2019-01-15 02:30:36 -0700

    Main project added.
    Basic sketchpad and grid resizing functionality added.
    Random color and shading abilities will be added in further iterations of the program.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# etch-a-sketch
+# Etch-A-Sketch
+JS based sketchpad/ etch-a-sketch hybrid. To use, open the .html file in an internet browser.
+
+With CSS grid, a square grid of divs is created within a container, and their individual attributes change upon interaction with the mouse pointer.
+
+You can also re-create the square grid with your own size parameters.
+
+This is a small project created to test the author in JS DOM manipulation.
+
+# Github Repository
+https://github.com/coped/etch-a-sketch
+
+# Author
+Dennis Cope, https://github.com/coped
---
+++ etch-a-sketch.js
@@ -0,0 +1,47 @@
+let gridContainer = document.querySelector('#grid');
+
+// Creation of initial grid
+let div;
+for (i = 0; i < 256; i++) {
+    div = document.createElement('div');
+    div.classList.add('emptySquare');
+    gridContainer.appendChild(div);
+}
+
+// Adding functionality to grid squares
+let gridDiv = Array.from(document.querySelectorAll('.emptySquare'));
+gridDiv.forEach(div => div.addEventListener('mouseover', addColor));
+
+
+function addColor(e) {
+    e.target.setAttribute('class', 'fillSquare');
+}
+
+function createGrid() {
+    let gridSize = +prompt('What size square grid? e.g. 12 x 12. (Only first value needed, values under 70 recommeded.)', '');
+    if (isNaN(gridSize) === true) {
+        alert('Enter a numerical value.');
+        createGrid();
+        return;
+    }
+    let gridArea = gridSize * gridSize;
+    gridContainer.remove();
+    gridContainer = document.createElement('div');
+    gridContainer.setAttribute('class', 'newGridContainer');
+    document.getElementById('container').appendChild(gridContainer);
+    gridContainer.style.gridTemplateColumns = 'repeat(' + gridSize + ', 1fr)';
+
+    for (i = 0; i < gridArea; i++) {
+        div = document.createElement('div');
+        div.classList.add('emptySquare');
+        gridContainer.appendChild(div);
+    }
+    let newGridDiv = Array.from(document.querySelectorAll('.emptySquare'));
+    newGridDiv.forEach(div => div.addEventListener('mouseover', addColor));
+
+}
+
+let button = document.querySelector('#new-grid');
+button.addEventListener('click', () => {
+    createGrid();
+});
